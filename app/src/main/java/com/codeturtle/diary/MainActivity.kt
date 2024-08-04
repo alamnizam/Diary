8dd20485d801c8c29f877640a080d7e4493b0275
witnip <70694885+witnip@users.noreply.github.com>
2024-08-05 04:52:22 +0530

    production created

--- app/src/main/java/com/codeturtle/diary/MainActivity.kt
+++ app/src/main/java/com/codeturtle/diary/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     DiaryTheme {
-        Greeting("Android")
+        Greeting("Android production")
     }
 }
