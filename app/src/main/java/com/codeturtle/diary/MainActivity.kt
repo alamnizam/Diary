0ed7745c5de64a1c9f34236d2d05c180787ff2f9
witnip <70694885+witnip@users.noreply.github.com>
2024-08-05 12:21:12 +0530

    Splash Screen added using splashScreen API

--- app/src/main/java/com/codeturtle/diary/MainActivity.kt
+++ app/src/main/java/com/codeturtle/diary/MainActivity.kt
@@ -3,7 +3,6 @@ package com.codeturtle.diary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.codeturtle.diary.ui.theme.DiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             DiaryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
