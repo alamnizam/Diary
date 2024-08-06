92dbd73e82f882b8fa588e53547ab9c0474d6d61
witnip <70694885+witnip@users.noreply.github.com>
2024-08-06 09:33:00 +0530

    Constants added for mongoDB

---
+++ app/src/main/java/com/codeturtle/diary/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.codeturtle.diary.util
+
+object Constants {
+    const val APP_ID ="mydiary-tbazoss"
+    const val CLIENT_ID = "671289794668-i8d6snlkbkdebemiq4hqggpph50oh69i.apps.googleusercontent.com"
+}
